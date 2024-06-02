4c5ab02f85391f3786e90f0da3322a3054374b26
llajos <18.lajos@gmail.com>
2024-06-02 15:50:01 +0200

    Chart setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useLayoutEffect, useState} from 'react';
 import './App.css';
+// import { DatePicker, TimeRangePickerProps } from "antd";
+import dayjs from 'dayjs';
+import { Line } from '@ant-design/plots';
 
 function App() {
+  const [data, setData] = useState<any[]>([]);
+  // const rangePresets: TimeRangePickerProps['presets'] = [
+  //   { label: 'Last 7 Days', value: [dayjs().add(-7, 'd'), dayjs()] },
+  //   { label: 'Last 14 Days', value: [dayjs().add(-14, 'd'), dayjs()] },
+  //   { label: 'Last 30 Days', value: [dayjs().add(-30, 'd'), dayjs()] },
+  //   { label: 'Last 90 Days', value: [dayjs().add(-90, 'd'), dayjs()] },
+  // ];
+
+  // const onRangeChange = (dates: null | (Dayjs | null)[], dateStrings: string[]) => {
+  //   if (dates) {
+  //     console.log('From: ', dates[0], ', to: ', dates[1]);
+  //     console.log('From: ', dateStrings[0], ', to: ', dateStrings[1]);
+  //   } else {
+  //     console.log('Clear');
+  //   }
+  // };
+
+  useLayoutEffect(() => {
+    const controller = new AbortController();
+
+    fetch('https://esp-sound-meter.balaton.workers.dev/', {signal: controller.signal})
+        .then((res) => res.json())
+        .then(res => {
+          setData(res.map((state: {timestamp: string, value: number}) => {
+              state.timestamp = dayjs(state.timestamp).format('YYYY-MM-DD HH:mm');
+              return state;
+          }));
+        });
+
+    // return () => {
+    //   controller.abort();
+    // }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<DatePicker.RangePicker presets={rangePresets} onChange={onRangeChange} />*/}
+      <Line
+          data={data}
+          x={{type: 'band'}}
+          className="chart"
+          xField="timestamp"
+          yField="value"
+          annotations={[{
+            type: "lineY",
+            yField: 50,
+            style: { stroke: "#FC2947", strokeOpacity: 1, lineWidth: 2 },
+          }]}
+          line={{
+            style: {
+              stroke: '#35374B',
+              strokeWidth: 2,
+            },
+          }}
+          tooltip={{
+            title: 'dBa',
+            items: [{
+              channel: 'x',
+              name: 'Timestamp',
+              color: '#fff',
+            }, {
+              channel: 'y',
+              name: 'dBa',
+              field: 'value',
+              valueFormatter: (value: number) => `${value} dBa`,
+            }],
+          }}
+      />
     </div>
   );
 }
