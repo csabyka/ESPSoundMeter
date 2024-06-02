864be78802cefe106fe6ef2e3cf0fdf1673ff4ee
llajos <18.lajos@gmail.com>
2024-06-02 15:52:08 +0200

    Antd ConfigProvider removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {ConfigProvider} from 'antd';
-import './index.css';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <ConfigProvider>
-          <App />
-      </ConfigProvider>
+      <App />
   </React.StrictMode>
 );
